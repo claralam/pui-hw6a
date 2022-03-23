07d4c3038e7f8d623660e5121bd516e4da9e5be3
Clara Lam <claralam@andrew.cmu.edu>
2022-03-22 22:52:18 -0400

    Added modal in covid results page

--- public/js/main.js
+++ public/js/main.js
@@ -16,3 +16,46 @@ function update_chosen_appt_time() {
     document.getElementById("appt-time").innerHTML = selectedTime.value;
     // console.log("time")
 }
+
+// For modals, opening popups
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+console.log(openModalButtons.length)
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log("clicked")
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    console.log("opening")
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
